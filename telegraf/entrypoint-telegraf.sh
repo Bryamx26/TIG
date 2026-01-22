50f69487c6e9750b7358c9d14aafddac67b0c8cb
Bryamx26 <144278205+Bryamx26@users.noreply.github.com>
2026-01-22 15:53:00 +0100

    ajout des logs docker

--- telegraf/entrypoint-telegraf.sh
+++ telegraf/entrypoint-telegraf.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # Attendre qu'InfluxDB soit prêt
-until curl -s http://influxdb2:8086/health | grep "pass"; do
+until curl -s http://influxdb2:8086/health | grep "pass" > /dev/null;do
   echo "Waiting for InfluxDB to be ready..."
   sleep 2
+
 done
 
-# Lancer Telegraf
 exec telegraf --config /etc/telegraf/telegraf.conf
